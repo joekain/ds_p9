9df1f8034360bb5c1ad84eabf60a15146da0ef7c
Joseph Kain <joekain@gmail.com>
2015-10-14 13:20:15 -0700

    Add twitter deps from previous iteration

--- apps/twitter/mix.exs
+++ apps/twitter/mix.exs
@@ -34,6 +34,12 @@ defmodule Twitter.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:oauth, github: "tim/erlang-oauth"},
+     {:extwitter, "~> 0.2"},
+     {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.2"},
+     {:httpotion, "~> 2.1.0"},
+     {:blocking_queue, "~> 1.0.0"},
+     {:poolboy,  github: "devinus/poolboy" },
+     {:exactor,  "~> 2.2"}]
   end
 end
