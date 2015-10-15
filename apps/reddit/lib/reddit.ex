327f7b5d2f5175a924ffe0b126ccf0ed419d3c98
Joseph Kain <joekain@gmail.com>
2015-10-14 21:47:31 -0700

    Retrieve oauth token from Reddit for script app

--- apps/reddit/lib/reddit.ex
+++ apps/reddit/lib/reddit.ex
@@ -1,2 +1,23 @@
 defmodule Reddit do
+  def get_oauth_token do
+    cfg = config
+
+    response = HTTPotion.post "https://www.reddit.com/api/v1/access_token", [
+      body: 'grant_type=password&username=#{cfg[:user]}&password=#{cfg[:pass]}',
+      headers: [
+        "User-Agent": "josephkain-test",
+        "Content-Type": "application/x-www-form-urlencoded"
+      ],
+      basic_auth: {cfg[:client_id], cfg[:secret]}
+    ]
+  end
+
+  defp config do
+    %{
+      user: System.get_env("REDDIT_USER"),
+      pass: System.get_env("REDDIT_PASSWORD"),
+      client_id: System.get_env("REDDIT_CLIENT_ID"),
+      secret: System.get_env("REDDIT_SECRET")
+    }
+  end
 end
