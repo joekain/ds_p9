88c7aaec901a058f5a40153635f806f7cfb1f86d
Joseph Kain <joekain@gmail.com>
2015-10-15 08:42:05 -0700

    Fetch articles and comments from subreddit

--- apps/reddit/lib/reddit.ex
+++ apps/reddit/lib/reddit.ex
@@ -5,7 +5,7 @@ defmodule Reddit do
       |> Poison.decode
     map["access_token"]
   end
-  
+
   defp request_oauth_token do
     cfg = config
 
@@ -26,5 +26,37 @@ defmodule Reddit do
       client_id: System.get_env("REDDIT_CLIENT_ID"),
       secret: System.get_env("REDDIT_SECRET")
     }
+  end
+
+  def get_new(token, subreddit) do
+    request("/r/#{subreddit}/new?limit=100", token)
+  end
+
+  def get_comments(token, subreddit, id) do
+    request("/r/#{subreddit}/comments/#{id}?limit=100", token)
+  end
+
+  defp request(endpoint, token) do
+    response = HTTPotion.get "https://oauth.reddit.com/" <> endpoint, [headers: [
+      "User-Agent": "josephkain-test/0.1 by josephkain",
+      "Authorization": "bearer #{token}"
+    ]]
+
+    {:ok, result} = response.body
+    |> Poison.decode
+
+    result
+  end
+
+  def test do
+    token = get_oauth_token
+    all = get_new(token, "programming")
+
+    all["data"]["children"]
+    |> Stream.map(fn item -> Map.get(item, "data") end)
+    |> Stream.map(fn item -> Map.get(item, "id") end)
+    |> Stream.map(fn id -> get_comments(token, "programming", id) end)
+    |> Stream.map(fn item -> IO.inspect item end)
+    |> Stream.run
   end
 end
