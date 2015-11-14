4b25acb696dd630a9c061c937bf041d030bc5d45
Joseph Kain <joekain@gmail.com>
2015-11-14 12:27:19 -0800

    Use ETS for the cache

--- apps/unshortening_pool/lib/unshortening_pool/cache.ex
+++ apps/unshortening_pool/lib/unshortening_pool/cache.ex
@@ -1,10 +1,17 @@
 defmodule UnshorteningPool.Cache do
   use ExActor.GenServer, export: __MODULE__
 
-  defstart start_link, do: initial_state(%{})
+  defstart start_link, do: initial_state(:ets.new(__MODULE__, []))
 
-  defcall check(url), state: state, do: reply(state[url] || false)
-  defcast add(short, long), state: state do
-    new_state Map.put(state, short, long)
+  defcall check(url), state: table do
+    case :ets.lookup(table, url) do
+      [{^url, long}] -> reply(long)
+      [] -> reply(false)
+    end
+  end
+
+  defcast add(short, long), state: table do
+    :ets.insert(table, {short, long})
+    new_state(table)
   end
 end
