b42e5890536eaadd2d5e2a53991aea5743fc6d36
Joseph Kain <joekain@gmail.com>
2015-11-18 13:32:42 -0800

    Refactor input, expected to be used in new tests

--- apps/unshortening_pool/test/unshortening_pool_test.exs
+++ apps/unshortening_pool/test/unshortening_pool_test.exs
@@ -2,21 +2,21 @@ defmodule UnshorteningPoolTest do
   use ExUnit.Case
   doctest UnshorteningPool
 
+  def input, do: [
+    "http://buff.ly/1LYD0tp",
+    "http://www.google.com",
+    "http://t.co/kfLrRZJ1cI",
+    "http://fb.me/6RK0qYjJI",
+  ]
+
+  def expected, do: [
+    "http://learningelixir.joekain.com/how-I-learned-elixir/?utm_content=buffer9a56c&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer",
+    "http://www.google.com",
+    "http://learningelixir.joekain.com/how-I-learned-elixir/?utm_content=buffer9a56c&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer",
+    "https://www.facebook.com/photo.php?fbid=863512007031067",
+  ] |> Enum.sort
+
   test "it should unshorten via the the pool" do
-    input = [
-      "http://buff.ly/1LYD0tp",
-      "http://www.google.com",
-      "http://t.co/kfLrRZJ1cI",
-      "http://fb.me/6RK0qYjJI",
-    ]
-
-    expected = [
-      "http://learningelixir.joekain.com/how-I-learned-elixir/?utm_content=buffer9a56c&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer",
-      "http://www.google.com",
-      "http://learningelixir.joekain.com/how-I-learned-elixir/?utm_content=buffer9a56c&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer",
-      "https://www.facebook.com/photo.php?fbid=863512007031067",
-    ] |> Enum.sort
-
     assert expected ==
       input
       |> UnshorteningPool.map
